e7ddc43aac11ee8295d7e447f207d161bb06ab0a
Instituto de Informática <idei.cefn@gmail.com>
2026-05-20 18:47:35 -0300

    fix: update init.sh permissions and remove unused symbolic link creation

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -11,14 +11,3 @@ if command -v mailpit &> /dev/null; then
 else
     echo "ADVERTENCIA: Mailpit no está instalado"
 fi
-
-# Crear enlaces simbólicos si los archivos existen
-if [ -f /workspaces/usim-framework/mainsync.sh ]; then
-    ln -sf /workspaces/usim-framework/mainsync.sh /usr/local/bin/mainsync
-    chmod +x /workspaces/usim-framework/mainsync.sh
-fi
-
-if [ -f /workspaces/usim-framework/migrate.sh ]; then
-    ln -sf /workspaces/usim-framework/migrate.sh /usr/local/bin/migrate
-    chmod +x /workspaces/usim-framework/migrate.sh
-fi
